a1cc8c7feecaca636df0257537610e05b4b12b69
Zikri Pratama <zikripratama723@gmail.com>
2026-07-12 17:14:45 +0700

    Add files via upload

---
+++ 2_TypeCasting.py
@@ -0,0 +1,14 @@
+Name = "john"
+age = 20 
+balence = 0
+wallet = 100.50
+working = True
+
+print(type(Name)) # maka dia akan menjadi <class 'str'>
+print(type(age)) # maka dia akan menjadi <class 'int'>
+print(type(wallet)) # maka dia akan menjadi <class 'float'>
+print(type(working)) # maka dia akan menjadi <class 'bool'>
+
+print(bool(balence)) # maka dia akan menjadi false karna tidak ada nilai nya
+print(bool(wallet)) # maka dia akan menjadi true karna ada nilai nya
+print(bool(working)) # maka dia akan menjadi true karna ada nilai nya
